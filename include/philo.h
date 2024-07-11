df47cf413cfdf463aa3a768cc577dd012fd69f68
DarkUnicorn42 <mateusz.wojtczak42@gmail.com>
2024-07-11 16:08:46 +0200

    piwo uciekam

--- include/philo.h
+++ include/philo.h
@@ -19,6 +19,27 @@
 # include <stdio.h>
 # include "../libft/libft.h"
 
+/*
+◦ number_of_philosophers: The number of philosophers and also the number
+of forks.
+◦ time_to_die (in milliseconds): If a philosopher didn’t start eating time_to_die
+milliseconds since the beginning of their last meal or the beginning of the simulation, they die.
+◦ time_to_eat (in milliseconds): The time it takes for a philosopher to eat.
+During that time, they will need to hold two forks.
+◦ time_to_sleep (in milliseconds): The time a philosopher will spend sleeping.
+◦ number_of_times_each_philosopher_must_eat (optional argument): If all
+philosophers have eaten at least number_of_times_each_philosopher_must_eat
+times, the simulation stops. If not specified, the simulation stops when a
+philosopher dies.
+*/
+typedef struct s_data
+{
+	int	number_of_philosophers;
+	int	time_to_die;
+	int	time_to_sleep;
+	int	number_of_times_each_philosopher_must_eat;
+} t_data
+
 
 
 #endif
--- src/philo.c
+++ src/philo.c
@@ -6,13 +6,55 @@
 /*   By: mwojtcza <mwojtcza@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/11 15:55:34 by mwojtcza          #+#    #+#             */
-/*   Updated: 2024/07/11 15:56:19 by mwojtcza         ###   ########.fr       */
+/*   Updated: 2024/07/11 16:08:16 by mwojtcza         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/philo.h"
 
+void	parse_arg(int ac, char **av, t_data data)
+{
+		
+}
+
 int	main(int ac, char **av)
 {
-	//things
+	int	number_of_philosophers;
+	int	time_to_die;
+	int	time_to_sleep;
+	int	number_of_times_each_philosopher_must_eat;
 }
+
+/*
+Your(s) program(s) should take the following arguments:
+number_of_philosophers time_to_die time_to_eat time_to_sleep
+[number_of_times_each_philosopher_must_eat]
+◦ number_of_philosophers: The number of philosophers and also the number
+of forks.
+◦ time_to_die (in milliseconds): If a philosopher didn’t start eating time_to_die
+milliseconds since the beginning of their last meal or the beginning of the simulation, they die.
+◦ time_to_eat (in milliseconds): The time it takes for a philosopher to eat.
+During that time, they will need to hold two forks.
+◦ time_to_sleep (in milliseconds): The time a philosopher will spend sleeping.
+◦ number_of_times_each_philosopher_must_eat (optional argument): If all
+philosophers have eaten at least number_of_times_each_philosopher_must_eat
+times, the simulation stops. If not specified, the simulation stops when a
+philosopher dies.
+• Each philosopher has a number ranging from 1 to number_of_philosophers.
+• Philosopher number 1 sits next to philosopher number number_of_philosophers.
+Any other philosopher number N sits between philosopher number N - 1 and philosopher number N + 1.
+
+About the logs of your program:
+• Any state change of a philosopher must be formatted as follows:
+◦ timestamp_in_ms X has taken a fork
+◦ timestamp_in_ms X is eating
+◦ timestamp_in_ms X is sleeping
+◦ timestamp_in_ms X is thinking
+◦ timestamp_in_ms X died
+Replace timestamp_in_ms with the current timestamp in milliseconds
+and X with the philosopher number.
+• A displayed state message should not be mixed up with another message.
+• A message announcing a philosopher died should be displayed no more than 10 ms
+after the actual death of the philosopher.
+• Again, philosophers should avoid dying
+*/
